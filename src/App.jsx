a330b8026788a646eb4d21e330ad99fb047bb134
Vivek Singh <viveksingh1035@gmail.com>
2025-11-29 02:46:46 +0530

    Moved Checkout Page into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
-import { Routes, Route } from 'react-router'
-import { HomePage } from './Pages/HomePage'
-import './Pages/HomePage.css'
-import './Pages/header.css'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './Pages/HomePage';
+import { CheckoutPage } from './Pages/CheckoutPage';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
+      <Route index element={<HomePage/>}/>
+      <Route path="checkout" element={<CheckoutPage/>} />
     </Routes>
   )
 }
