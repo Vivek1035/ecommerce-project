c2430a2fdcfd1e3e0550eb550dc77022932e3236
Vivek Singh <viveksingh1035@gmail.com>
2025-11-30 02:22:53 +0530

    Add PageNotFound Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { HomePage } from './Pages/HomePage';
 import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { OrdersPage } from './Pages/OrdersPage';
 import { TrackingPage } from './Pages/TrackingPage';
+import { PageNotFound } from './Pages/PageNotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage/>} />
       <Route path="orders" element={<OrdersPage/>} />
       <Route path="tracking" element={<TrackingPage/>} />
+      <Route path="*" element={<PageNotFound/>} />
     </Routes>
   )
 }
