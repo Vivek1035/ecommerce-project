256124379aa0ed78b3e0f90310bd88560f5a6d39
Vivek Singh <viveksingh1035@gmail.com>
2025-12-03 01:03:58 +0530

    Use Axios to fetch data and use data from backend to generate HTML

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,18 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './HomePage.css';
-import { products } from '../../starting-code/data/products';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import { Header } from '../components/Header';
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
-        .then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-        })
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data);
+        });
+    }, []);
 
     return (
         <>
