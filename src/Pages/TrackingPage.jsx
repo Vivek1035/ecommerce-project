47c6763fb9adbe984a655926f4708cd8ac78e65f
Vivek Singh <viveksingh1035@gmail.com>
2026-01-08 02:00:52 +0530

    Tracking Page Status Update Fixed

--- src/Pages/TrackingPage.jsx
+++ src/Pages/TrackingPage.jsx
@@ -31,6 +31,9 @@ export function TrackingPage({ cart }) {
     let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
     if (deliveryPercent > 100) deliveryPercent = 100;
 
+    const isPreparing = deliveryPercent < 33;
+    const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+    const isDelivered = deliveryPercent === 100;
 
     return (
         <>
@@ -61,13 +64,13 @@ export function TrackingPage({ cart }) {
                     <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
-                        <div className="progress-label">
+                        <div className={`progress-label ${isPreparing && 'current-status'}`}>
                             Preparing
                         </div>
-                        <div className="progress-label current-status">
+                        <div className={`progress-label ${isShipped && 'current-status'}`}>
                             Shipped
                         </div>
-                        <div className="progress-label">
+                        <div className={`progress-label ${isDelivered && 'current-status'}`}>
                             Delivered
                         </div>
                     </div>
