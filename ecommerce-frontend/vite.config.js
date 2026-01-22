0d6dafa4e2d97defc545f6ec47a83bc75622f815
Vivek Singh <viveksingh1035@gmail.com>
2026-01-22 11:48:26 +0530

    Create new React Typescript project

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
       },
       '/images': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:8080',
       },
     },
   },
